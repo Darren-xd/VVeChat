05462999adc82d52dfec0bcc5d1a28929fdfb036
xude <874111575@qq.com>
2016-03-03 23:20:10 +0800

    创建项目

---
+++ VVeChat/VVeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VVeChat
+//
+//  Created by Darren on 16/3/3.
+//  Copyright © 2016年 darren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VVeChat/VVeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VVeChat
+//
+//  Created by Darren on 16/3/3.
+//  Copyright © 2016年 darren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
